ba20d8cb852eceb590ed3bea314f17896b671a5b
Rodrigo Lira <8973745+rodrigolira@users.noreply.github.com>
2022-01-09 11:11:16 -0300

    add sample project using a custom configuration builder using static data

---
+++ MyCustomConfigurationBuilders/StaticDataConfigurationBuilder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Configuration.ConfigurationBuilders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyCustomConfigurationBuilders
+{
+    public class StaticDataConfigurationBuilder : KeyValueConfigBuilder
+    {
+        public override ICollection<KeyValuePair<string, string>> GetAllValues(string prefix)
+        {
+            return GetAllSettings().Where(ev => ev.Key.StartsWith(prefix, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+        public override string GetValue(string key)
+        {
+            string value;
+            return GetAllSettings().TryGetValue(key, out value) ? value : null;
+        }
+
+        private Dictionary<string, string> GetAllSettings()
+        {
+            return new Dictionary<string, string>
+            {
+                { "AppSettings_MyCustomSetting", "This data has been set using a custom configuration builder" }
+            };
+        }
+    }
+}
---
+++ SimpleCustomConfigurationBuilder/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleCustomConfigurationBuilder.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string someAppSetting = ConfigurationManager.AppSettings["MyCustomSetting"] ?? "Not found";
+            string someOtherAppSetting = ConfigurationManager.AppSettings["SomeOtherSettingThatDoesntExist"] ?? "Not found";
+
+            return Json(new { someAppSetting, someOtherAppSetting }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
